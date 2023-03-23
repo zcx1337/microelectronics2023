84ad241194686dc527e7403831d3f130dddaa21e
zcx1337 <nemail.mir@gmail.com>
2023-03-23 13:33:23 +0300

    add vhdl alg impl

--- 5/vhdl/odd_mult.vhd
+++ 5/vhdl/odd_mult.vhd
@@ -1,20 +1,28 @@
+package odd_mult_types is
+subtype vector_address is natural range 0 to 4;
+type vector_t is array (vector_address) of integer;
+end odd_mult_types;
+
 entity odd_mult is
-    Port ( vector : in  std_logic_vector(4 downto 0);
-           result : out integer);
+    Port
+    (
+        vector : in  odd_mult_types.vector_t;
+        result : out integer
+    );
 end odd_mult;
 
 architecture Behavioral of odd_mult is
 begin
-
     process(vector)
         variable odd_product : integer := 1;
     begin
         for i in vector'range loop
             if (vector(i) mod 2 = 1) then
-                odd_product := odd_product * to_integer(unsigned(vector(i)));
+                odd_product := odd_product * vector(i);
             end if;
         end loop;
         result <= odd_product;
+        odd_product := 1;
     end process;
-
 end Behavioral;
+
